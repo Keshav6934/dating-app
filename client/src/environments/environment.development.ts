37e13afa1ab7ec28dc61f9068e9e7944a1b7de23
Keshav Sharma <keshavsharma6934@gmail.com>
2026-02-16 10:55:39 +0530

    End of section 9

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+import { flush } from "@angular/core/testing";
+
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:7022/api/'
+};
---
+++ client/src/types/member.ts
@@ -0,0 +1,19 @@
+﻿export type Member =  {
+    id: string
+    dateOfBirth: string
+    imageUrl?: string
+    displayName: string
+    created: string
+    lastActive: string
+    gender: string
+    description?: string
+    city: string
+    county: string
+}
+
+export type Photo = {
+    id: number
+    url: string
+    publicId?: string
+    memberId: string
+}
